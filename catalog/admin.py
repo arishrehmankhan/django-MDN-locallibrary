e9a511dc383b52818a73be616240b91390be55d7
Arish Rehman Khan <arish.rehman.khan@gmail.com>
2020-02-10 12:56:58 +0530

    changed admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
-
+from .models import Author, Genre, Book, BookInstance, Language
 # Register your models here.
+
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        })
+    )
+
+# admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Author, AuthorAdmin)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
--- catalog/models.py
+++ catalog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from datetime import date
 from django.urls import reverse
+from django.contrib.auth.models import User
 import uuid
 
 # Create your models here.
 class Genre(models.Model):
     name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter the book's natural language (e.g. English, French, Japanese etc.")
 
     def __str__(self):
         return self.name
@@ -16,7 +23,12 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+
+    def display_genre(self):
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
 
     def __str__(self):
         return self.title
@@ -29,6 +41,13 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    # borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -66,8 +85,3 @@ class Author(models.Model):
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
 
-class Language(models.Model):
-    name = models.CharField(max_length=200, help_text="Enter the book's natural language (e.g. English, French, Japanese etc.")
-
-    def __str__(self):
-        return self.name
