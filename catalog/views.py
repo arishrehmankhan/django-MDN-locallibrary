ba5ab5d6df3919c246a73e2e1f24d64820ed13b0
Arish Rehman Khan <arish.rehman.khan@gmail.com>
2020-02-10 13:30:28 +0530

    created homepage

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world')
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    num_genre = Genre.objects.all().count()
+    num_books_containing_of = Book.objects.filter(title__icontains='of').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genre': num_genre,
+        'num_books_containing_of': num_books_containing_of,
+    }
+
+    return render(request, 'index.html', context=context)
